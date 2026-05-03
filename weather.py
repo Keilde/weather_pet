332540b044947374475ef158744cba709caea580
Keilde <osipov-nik2002@mail.ru>
2026-05-04 01:19:43 +0300

    2-nd fix

--- weather.py
+++ weather.py
@@ -1,12 +1,21 @@
 import os
 import datetime
 import requests
+import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor 
+from aiogram.filters import CommandStart 
 
-bot = Bot(token='1')
-dp= Dispatcher(bot)
+bot = Bot(token='8712024753:AAFLeLHhv_sfVV8qfb123nc_PLFZfcesWHE')
+dp= Dispatcher()
 
-@dp
-print("Сегодня матчей нет.") 
+@dp.message(CommandStart())
+async def start_command(message: types.Message):
+    await message.reply("Chouse your city")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__=="__main__":
+    asyncio.run(main())
+
 
